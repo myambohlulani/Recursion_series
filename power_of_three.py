990c38165b0f4ce00f3c034816735b32db729803
0mniManTheCpu <mymhlu001@myuct.ac.za>
2025-05-24 21:22:55 +0200

    ahh this one is the end of the power thingy, It is time to face the real recursion problems

--- power_of_three.py
+++ power_of_three.py
@@ -3,17 +3,16 @@
 # 23 May 2025
 
 def power_of_three(number: int) -> bool:
-    if not number:
+    if not number  or (number < 3 and number != 1):
         return False
-    if number == 0:
+    if number % 3 == 0 or number == 1:
         return True
-    if number < 3:
+    if number % 3 != 0:
         return False
-    if number % 3 == 0:
-        return True
-    return power_of_three(number // 2)
+    return power_of_three(number // 3)
 
 print(power_of_three(10)) # False
 print(power_of_three(3)) # True
 print(power_of_three(27)) # True
-print(power_of_three(1)) # False
+print(power_of_three(1)) # True
+print(power_of_three(0)) # False
